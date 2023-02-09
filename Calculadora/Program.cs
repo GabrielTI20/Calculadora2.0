5247c68ee8e7f1faa6b5cbdf08695bbd845a7a43
GabrielTI20 <120021921+GabrielTI20@users.noreply.github.com>
2023-02-09 12:00:46 -0300

    Add files via upload

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculadora
@@ -10,36 +11,118 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            
             double Valor1 = 0;
             double Valor2 = 0;
-            double opcao = 0;
+            string i = ""; 
+           
             
-            //Está a exibir na tela para usúario
-            Console.WriteLine("Digite o primeiro número: ");
+            while (i != "2")
+            {
+               
+                Console.WriteLine("Digite o primeiro número: ");
+            
+                Valor1=Convert.ToDouble(Console.ReadLine());
 
-            //Convert.ToDouble "Está convertendo uma string em double"
-            //Console.ReadLine "Abre caixa texto para usuário digitar"
-            Valor1=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o segundo número: ");
 
-            //Está a exibir na tela para usúario
-            Console.WriteLine("Digite o segundo número: ");
+            
+                Valor2 = Convert.ToDouble(Console.ReadLine());
 
-            //Convert.ToDouble "Está convertendo uma string em double"
-            //Console.ReadLine "Abre caixa texto para usuário digitar"
-            Valor2 = Convert.ToDouble(Console.ReadLine());
-
-            // "/n" pula uma linha "/r" pula pra ultima linha
-            Console.WriteLine("Qual operação você quer fazer? "  + "\n" 
+            
+            
+                Console.WriteLine("Calculadora " + "\n" + "\n"
                + "       1-Soma" + "\n" 
                + "       2-Subtração" +"\n" 
                + "       3-Divisão" + "\n" 
                + "       4-Multiplicação" + "\n" 
-               + "       5-Nada, só estou dando uma olhadinha " + "\n");
+               + "       5-Nada, só estou dando uma olhadinha " + "\n" + "\n"
+               + "Qual operação você quer fazer? ");
 
+
+                switch (Console.ReadLine())
+                {
+
+
+                    case "1":
+                        Console.WriteLine("Valor da soma é: " + (Valor1 + Valor2) + "\n");
+
+                        break;
+
+
+                    case "2":
+                        Console.WriteLine("Valor da subtração é:" + (Valor1 - Valor2) + "\n");
+
+                        break;
+
+
+                    case "3":
+
+                        if (Valor1 >= Valor2)
+                        {
+                            Console.WriteLine("Valor da divisão é: " + (Valor1 / Valor2) + "\n");
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Invalido." + "\n");
+
+                        }
+
+                        break;
+
+
+                    case "4":
+
+                        Console.WriteLine("Valor da multiplicação é: " + (Valor1 * Valor2) + "\n");
+
+                        break;
+
+
+                    case "5":
+
+
+                        break;
+
+                }
+                Console.WriteLine("Quer continuar com a operações"
+                + "\n" + "        1-sim"
+                + "\n" + "        2-Não");
+                i=Console.ReadLine();
+                
+
+            }
+            
+            
+            
+            
+            
+
+
+
+        }
+
+    }
+}
+//"Double, int , string " e o tipo da variavel 
+
+//Console.WhiteLine "Está a exibir na tela para usúario"
+
+//Convert.ToDouble "Está convertendo uma string em double"
+
+//Console.ReadLine "Abre caixa texto para usuário digitar"
+
+// "/n" pula uma linha "/r" pula pra ultima linha
+
+
+
+/*                                                                                                                          "Escondido"
             //Convert.ToDouble "Está convertendo uma string em double"
             opcao = Convert.ToDouble (Console.ReadLine());
             
-            // "/if" condição 
+            
+            // "/if" condição de verificação
             if(opcao== 1) 
             {
                 Console.WriteLine("Valor da soma é: " + (Valor1 + Valor2));
@@ -68,12 +151,5 @@ namespace Calculadora
             else if (opcao==5) 
             {
                 Console.WriteLine("Aperte enter para fechar!");
-            }
-
-
-
-
-        }
-
-    }
-}
+            }                                                                                                                           "Escondido"
+            */
